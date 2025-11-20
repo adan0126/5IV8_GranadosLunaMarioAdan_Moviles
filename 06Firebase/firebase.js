3e8a0bfac3fe083f72451e2efc8721a57f91256a
adan0126 <mgranadosl2301@alumno.ipn.mx>
2025-11-19 19:58:52 -0600

    Se creo la practica para conectar la BD en una aplicación movil, con firebase

---
+++ 06Firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmHpPPk4VP-8Wht9hi8YVh8C1Z_iVmAg0",
+  authDomain: "bd-movil-5d9a5.firebaseapp.com",
+  projectId: "bd-movil-5d9a5",
+  storageBucket: "bd-movil-5d9a5.firebasestorage.app",
+  messagingSenderId: "59509677368",
+  appId: "1:59509677368:web:54758ed4197b97dec275cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
